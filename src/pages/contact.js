19cfc0dbfcc720acd2886f201c720a6793a4ed56
Reed Debaets <reed.debaets@gmail.com>
2021-04-29 22:50:01 +0200

    contact form UX improvements

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,24 @@
-import * as React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Button, Form, Input } from "antd"
 import axios from "axios"
-
 const { TextArea } = Input
 
-const page = ({data}) => {
-  const onFinish = (values) => {
-    axios.post('/api/SendMail',values)
-  };
+const Page = ({data}) => {
+  const [formAction,setFormAction] = useState({action: "Send", disabled:false})
 
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
+  const onFinish = (values) => {
+    setFormAction({action: "Please wait...", disabled:true})
+    axios.post('/api/SendMail',values)
+    .then((res) => {
+      setFormAction({action: "Sent!", disabled:true})
+    }).catch((ex) => {
+      setFormAction({action: "Send", disabled:false})
+    })
+  }
+
+  const onFinishFailed = (errorInfo) => {}
 
   return <>
     <a href={data.staticMap.mapUrl}>
@@ -30,17 +35,17 @@ const page = ({data}) => {
       <li>You can write us at PO Box 213, South Hill, VA 23970</li>
       <li>You can e-mail us using the form below.</li>
     </ul>
-    <Form labelCol={0} wrapperCol={16} name="contact" onFinish={onFinish} onFinishFailed={onFinishFailed} >
+    <Form labelCol={0} wrapperCol={16} name="contact" onFinish={onFinish} onFinishFailed={onFinishFailed}>
       <Form.Item name="Name" rules={[{required:true}]}>
-        <Input placeholder="Enter your name..." />
+        <Input placeholder="Enter your name..." disabled={formAction.disabled} />
       </Form.Item>
       <Form.Item name="Email" rules={[{required:true}]}>
-        <Input placeholder="Enter your email..." type="email" />
+        <Input placeholder="Enter your email..." type="email" disabled={formAction.disabled} />
       </Form.Item>
       <Form.Item name="Message" rules={[{required:true}]}>
-        <TextArea showCount maxLength={2000} placeholder="Message" autoSize={{minRows:3, maxRows:15}}/>
+        <TextArea showCount maxLength={2000} placeholder="Message" autoSize={{minRows:3, maxRows:15}} disabled={formAction.disabled} />
       </Form.Item>
-      <Button htmlType="submit" block>Send</Button>
+      <Button htmlType="submit" block disabled={formAction.disabled}>{formAction.action}</Button>
     </Form>
   </>
 };
@@ -56,4 +61,4 @@ export const query = graphql`query {
   }
 }`
 
-export default page
+export default Page
