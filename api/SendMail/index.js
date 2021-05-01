44d6445f953110e8ee9c888ecc6e44ac366287ed
Reed Debaets <reed.debaets@gmail.com>
2021-05-01 18:14:41 +0200

    bugfix email function

--- api/SendMail/index.js
+++ api/SendMail/index.js
@@ -2,20 +2,16 @@ const fetch = require('node-fetch');
 const sgMail = require('@sendgrid/mail');
 
 module.exports = async function (context, req) {
-    context.log(process.env.NODE_ENV)
-    sgMail.setApiKey(process.env.sendgrid_key);
-    const name = req.query.name || (req.body && req.body.name)
-    const email = req.query.body || (req.body && req.body.email)
-    const message = req.query.message || (req.body && req.body.message)
-
-    const res = await sgMail.send({
+    sgMail.setApiKey(process.env.sendgrid_key)
+    if(!req.body) throw 'Bad Request'
+    await sgMail.send({
         "to": process.env.sendgrid_contact_recipient,
         "from": "admin@southhillbreadbox.com",
         "subject": "Contact request submitted from SouthhillBreadBox.com",
-        "text": `name: ${name}
-email: ${email}
+        "text": `name: ${req.body.Name}
+email: ${req.body.Email}
 ---
-${message}`,
+${req.body.Message}`,
         "mailSettings": {
             "sandboxMode": {
                 "enable": process.env.NODE_ENV == "development" ? true : false
